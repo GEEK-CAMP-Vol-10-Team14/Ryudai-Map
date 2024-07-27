24e4dcbdb2fafea9c2177ffe161cb789d464c752
Takuya Chinen <e245745@ie.u-ryukyu.ac.jp>
2024-07-28 07:13:38 +0900

    add ルーティングを設定

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import { Map } from "./components/Map";
+import { Routes, Route } from 'react-router-dom';
+import { Building_One } from './pages/Building_One'
+import { Building_Two } from './pages/Building_Two'
+import { Building_Three } from './pages/Building_Three';
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      <Routes>
+        <Route path='/' element={ <Map /> } />
+        <Route path='/Building_One' element={ <Building_One /> } />
+        <Route path='/Building_Two' element={ <Building_Two /> } />
+        <Route path='/Building_Three' element={ <Building_Three /> } />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Building_Three.js
@@ -0,0 +1,8 @@
+// src/routes/Building_Three.js
+export const Building_Three = () =>{
+    return (
+        <div>
+            <p>３号館</p>
+        </div>
+    )
+}
