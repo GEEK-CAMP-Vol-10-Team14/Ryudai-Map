c2105f952504fd0a10daf2c84e7da8d2900b858e
Takuya Chinen <e245745@ie.u-ryukyu.ac.jp>
2024-07-27 16:52:45 +0900

    add 地図を画面全体に表示

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './Map.css';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -32],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+
 
 export const Map = () => {
   // 緯度軽度
-  const position = [51.505, -0.09];
+    const position = [26.252590, 127.766111];
   // 初期マップズームレベル
-  const zoom = 13;
-  return (
+    const zoom = 18;
+    return (
     <MapContainer center={position} zoom={zoom}>
-      <TileLayer
+        <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    />
+        <Marker position={position}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
     </MapContainer>
-  )
+)
 };
