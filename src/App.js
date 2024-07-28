c229b5dbfe002174a7f521440a17f92629fc394c
Takuya Chinen <e245745@ie.u-ryukyu.ac.jp>
2024-07-28 09:17:33 +0900

    fix ファイル名

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import 'leaflet/dist/leaflet.css';
 import { Map } from "./components/Map";
 import { Routes, Route } from 'react-router-dom';
-import { Building_One } from './pages/Building_One'
-import { Building_Two } from './pages/Building_Two'
-import { Building_Three } from './pages/Building_Three';
+import { BuildingOne } from './pages/BuildingOne'
+import { BuildingTwo } from './pages/BuildingTwo'
+import { BuildingThree } from './pages/BuildingThree';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={ <Map /> } />
-        <Route path='/Building_One' element={ <Building_One /> } />
-        <Route path='/Building_Two' element={ <Building_Two /> } />
-        <Route path='/Building_Three' element={ <Building_Three /> } />
+        <Route path='/BuildingOne' element={ <BuildingOne /> } />
+        <Route path='/BuildingTwo' element={ <BuildingTwo /> } />
+        <Route path='/BuildingThree' element={ <BuildingThree /> } />
       </Routes>
     </div>
   );
---
+++ src/pages/BuildingTwo.js
@@ -0,0 +1,8 @@
+// src/routes/BuildingTwo.js
+export const BuildingTwo = () =>{
+    return (
+        <div>
+            <p>２号館</p>
+        </div>
+    )
+}
